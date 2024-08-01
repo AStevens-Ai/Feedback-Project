081dc4096c0234a65f3d32861a5c4fb58c41a7e3
aStevens <astevens@gmail.com>
2024-08-01 01:19:14 -0400

    created cards , amount of reviews and average rating.

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import FeedbackData from './data/FeedbackData';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+
+function MyApp() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const DeleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(
+        feedback.filter(() => (item) => {
+          item.id != id;
+        })
+      );
+    }
+  };
+
+  return (
+    <>
+      <Header text="Hello World" />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={DeleteFeedback} />
+      </div>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MyApp from './App';
+import './index.css';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<MyApp />);
